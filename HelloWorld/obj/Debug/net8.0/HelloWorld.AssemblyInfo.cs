7e2937cce03660aa9fd4eab834ce9a185f22166b
Dee J. Mann <dee.j.mann@hotmail.com>
2024-01-10 16:51:58 -0500

    Added Mastering-RegEx/Commafy

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d115c2f8426ace8a1ebb7f84ff4a24672666a04d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Mastering-RegEx/Commafy/Commafy/Program.cs
@@ -0,0 +1,39 @@
+﻿/* This example is modified from the examples in O'Reilly's Mastering Regular Expressions
+*/
+
+// See https://aka.ms/new-console-template for more information
+using System;
+// Import the RegEx module
+using System.Text.RegularExpressions;
+
+// Initialize our string to blank
+string strNumber = "";
+
+Console.WriteLine("Please enter a number greater than 999.  The larger the number, the better: ");
+
+// Get the input from the user
+strNumber = Console.ReadLine();
+
+/* RegEx:
+    @"" - C# - Designates this as a RegEx pattern
+    ^   - From the beginning of the string
+    \d{4,} - Match 4 or more digits
+    $   - To the end of the string (ensuring there are no other characters)
+*/
+if(Regex.IsMatch(strNumber, @"^\d{4,}$")) {
+    // Insert the commas into the number
+    /* RegEx:
+        ?<= - Positive look behind (can we match this pattern before our current position)
+        \d  - Match exactly 1 digit
+        ?=  - Positive look ahead (can we match this pattern after our current position)
+        ()  - Subexpression group
+        (?: - Do NOT assign this group to a variabel (TODO:  Verify if these backreferences are assigned to variables as in Perl)
+        \d{3} - Match exactly 3 digits
+        ?!  - Negative look ahead (does the pattern after our current position no match)
+    */
+    strNumber = Regex.Replace(strNumber, @"(?<=\d)(?=(?:\d{3})+(?!\d))", ",");
+
+    Console.WriteLine(strNumber);
+} else {
+    Console.WriteLine("Please enter at least 4 ditits!");
+}
