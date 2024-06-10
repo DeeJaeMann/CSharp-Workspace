dcb7a066e51c19ee7c4513801fd6b3f62653de82
Dee J. Mann <dee.j.mann@hotmail.com>
2024-06-10 09:18:19 -0400

    Removed HelloWorld old version

--- HelloWorld/README.md
+++
@@ -1,4 +0,0 @@
-# HelloWorld
-
-This is just a simple project to inspect all of the aspects of a simple C# Project.
-Just a typical Hello World example generated from the console app project template.
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2937cce03660aa9fd4eab834ce9a185f22166b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eddb4918c1928dc6d6100cd59c431f580c18c4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
