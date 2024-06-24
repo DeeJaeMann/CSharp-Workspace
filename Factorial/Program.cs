01cd96041988ef16e54fb3f397a232a6e8914c84
Dee J. Mann <dee.j.mann@hotmail.com>
2024-06-24 09:49:09 -0400

    Added Console statements and TryParse

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Please enter a number: ");
+int number;
+string input = Console.ReadLine();
+
+int Factorial(int number)
+{
+    return 0;
+}
+
+if ( int.TryParse(input, out number) )
+{
+    Console.WriteLine($"You entered {input}");
+    Console.WriteLine($"Factorial: {Factorial(number)}");
+}
+else
+{
+    Console.WriteLine("That is not a number!");
+}
+
+
