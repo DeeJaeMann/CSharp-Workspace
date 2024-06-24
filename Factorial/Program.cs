557cd23906000c77b541c57ce97bd7c686b32b53
Dee J. Mann <dee.j.mann@hotmail.com>
2024-06-24 10:30:35 -0400

    Moved function outside of Main. Added function documentation

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,30 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Please enter a number: ");
-int number;
-string input = Console.ReadLine();
-
-int Factorial(int number)
+﻿internal class Program
 {
-    int result = 1;
-    Console.WriteLine($"Number is {number}");
+    /// <summary>
+    /// Performs factorial calculation on number.
+    /// </summary>
+    /// <param name="number">The number.</param>
+    /// <returns>
+    /// The result of the calculation as an int.
+    /// </returns>
+        static int Factorial(int number)
+        {
+            int result = 1;
 
-    for ( int thisNumber = number; thisNumber > 0; thisNumber--)
+            for (int thisNumber = number; thisNumber > 0; thisNumber--)
+            {
+                result *= thisNumber;
+            }
+
+            return result;
+        }
+    private static void Main(string[] args)
     {
-        result *= thisNumber;
-        Console.WriteLine($"thisNumber {thisNumber} result {result}");
+        Console.Write("Please enter a number: ");
+        int number;
+        string input = Console.ReadLine();
+
+
+        if (int.TryParse(input, out number))
+        {
+            Console.WriteLine($"You entered {input}");
+            Console.WriteLine($"Factorial: {Factorial(number)}");
+        }
+        else
+        {
+            Console.WriteLine("That is not a number!");
+        }
     }
-
-    return result;
 }
-
-if ( int.TryParse(input, out number) )
-{
-    Console.WriteLine($"You entered {input}");
-    Console.WriteLine($"Factorial: {Factorial(number)}");
-}
-else
-{
-    Console.WriteLine("That is not a number!");
-}
-
-
